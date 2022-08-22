3cde6c458f2610a4657af0f2cf71af513ff0c94f
xiaoniu666888 <107358468+xiaoniu666888@users.noreply.github.com>
2022-08-23 00:15:08 +0800

    Add files via upload

---
+++ 力扣/两两交换链表中的节点.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var swapPairs = function(head) {
+
+    let dummy = new ListNode();
+     dummy.next = head;
+    let current = dummy; 
+    while(current.next !== null && current.next.next !== null){
+        let n1 = current.next;
+        let n2 = current.next.next;
+        current.next = n2;
+        n1.next = n2.next;
+        n2.next = n1;
+        current = n1;
+    }
+    return dummy.next
+};
